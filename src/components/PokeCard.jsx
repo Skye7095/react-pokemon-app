8ec60ef410920d1f54da0256d93a41c51fc8947b
Skye <hkim7095@gmail.com>
2023-09-15 01:19:11 +0900

    DetailPage Basic UI

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import LazyImage from './LazyImage'
+import { Link } from 'react-router-dom'
 
 const PokeCard = ({url, name}) => {
   const [pokemon, setPokemon] = useState()
@@ -37,8 +38,8 @@ const PokeCard = ({url, name}) => {
   return (
     <>
       {pokemon && 
-        <a 
-          href={`/pokemon/${name}`} 
+        <Link 
+          to={`/pokemon/${name}`} 
           className={`box-boder rounded-lg ${border} w-[8.5rem] h-[8.5rem] z-0 bg-slate-800 justify-between items-center`} 
         >
           <div className={`${text} h-[1.5rem] text-xs w-full pt-1 px-2 text-right rounded-t-lg`}>
@@ -52,10 +53,10 @@ const PokeCard = ({url, name}) => {
               />
             </div>
           </div>
-          <div className={`${bg} text-xs text-zinc-100 h-[1.5rem] rounded-b-lg uppercase font-medium pt-1`}>
+          <div className={`${bg} text-center text-xs text-zinc-100 h-[1.5rem] rounded-b-lg uppercase font-medium pt-1`}>
             {pokemon.name}
           </div>
-        </a>
+        </Link>
       }
     </>
   )
