675a8e0a744244d3f5a4b92499ed370be563e819
Skye <hkim7095@gmail.com>
2023-09-17 23:48:32 +0900

    Log In / Out

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom'
+import NavBar from './components/NavBar'
 import MainPage from './pages/MainPage'
+import LoginPage from './pages/LoginPage'
 import DetailPage from './pages/DetailPage'
+
+const Layout = () => {
+  return (
+    <>
+      <NavBar />
+      <br />
+      <br />
+      <br />
+      <Outlet />
+    </>
+  )
+}
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<MainPage />} />
-        <Route path='/pokemon/:id' element={<DetailPage />} />
+        <Route path='/' element={<Layout />}>
+          <Route path='/' element={<MainPage />} />
+          <Route path='login' element={<LoginPage />} />
+          <Route path='/pokemon/:id' element={<DetailPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
